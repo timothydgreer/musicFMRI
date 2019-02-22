120e4f4cee6526e7c06d66ed8ee2600906850dca
timothydgreer <timothydgreer@gmail.com>
2019-02-22 14:41:39 -0800

    Fixing peaks

--- find_spikes.m
+++ find_spikes.m
@@ -1,5 +1,8 @@
-function above_thresh_peak_inds = find_spikes(new_signal,thresh)
+function above_thresh_peak_inds = find_spikes(new_signal,thresh,plotTrue)
     above_thresh = find(new_signal>thresh,2000000000);
     [pks,locs] = findpeaks(new_signal,1:length(new_signal));
     above_thresh_peak_inds = intersect(locs,above_thresh);
+    if plotTrue == true
+        plot(1:length(above_thresh_peak_inds),above_thresh_peak_inds)
+    end
 end
